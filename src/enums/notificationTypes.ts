adfad7ce55973b0e4032926ec78dc9fffc6655a9
Antony Raj <antonyraj@Antonys-MacBook-Air.local>
2021-10-03 14:03:09 +0530

    App: Code clean up

--- src/enums/notificationTypes.ts
+++
@@ -1,7 +0,0 @@
-export enum NotificationTypes {
-    SUCCESS = "success",
-    ERROR = "error",
-    WARNING = "warning",
-    INFO = "info",
-    OPEN = "open"
-}
--- src/routes/apiRoutes.ts
+++
@@ -1,3 +0,0 @@
-export const ApiRoutes = {
-    BASE_URL: "https://maps.googleapis.com/maps/api/js?key=API_KEY&libraries=places"
-}
